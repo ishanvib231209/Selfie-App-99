f450b62860d1e66c63530720c3dc85270c95b6c5
ishanvib231209 <76564418+ishanvib231209@users.noreply.github.com>
2021-06-21 16:38:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,16 +13,27 @@ console.log(event);
 var Content = event.results[0][0].transcript;
 console.log(Content);
 
-document.getElementById("textbox").innerHTMl = Content;
-speak();
+document.getElementById("textbox").innerHTML = Content;
+if(Content=="Take my selfie."){
+    console.log("Taking Selfie--");
+    speak();
+
+}
+
 }
 
 function speak(){
     var synth = window.speechSynthesis;
-    speak_data= document.getElementById("textbox").value;
+    speak_data= "taking your selfie in 5 seconds"
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis); 
     Webcam.attach(camera);
+
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    }, 5000);
+    
 }
 
 Webcam.set({
@@ -32,3 +43,16 @@ Webcam.set({
     png_quality:90
 });
 camera= document.getElementById("camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src ;
+    link.href = image;
+    link.click();
+}
